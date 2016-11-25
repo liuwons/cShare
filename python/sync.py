8e88028aa5fcc65e03c59ccafc4454b8611e0ba4
liuwons <liuwons@163.com>
2016-11-25 15:34:37 +0800

    init

---
+++ python/sync.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Sync:
+    def __init__(self):
+        self.remote_host = 'lwons.com'
+
+    def upload(self):
+        pass
+
+    def download(self):
+        pass
+
+    def run(self, mode):
+        pass
